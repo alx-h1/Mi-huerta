aa27c39e2a23b7e473ad9ecebc7a50cf278c2435
alx-h1 <alexishern@gmail.com>
2026-08-26 11:08:17 -0600

    Refresh PWA cache for shortcut setup flow

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='mi-huerta-v3';
+const CACHE='mi-huerta-v4';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./recommendations.js','./daily.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
